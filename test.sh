53cc5a63421acabc1b6645087168611054415894
William Leese <wleese@bol.com>
2025-12-30 08:37:58 +0100

    cleanup

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -x
 
-./mvnw org.openrewrite.maven:rewrite-maven-plugin:run \
-    -Drewrite.configLocation=src/main/resources/test.yaml \
-    -Drewrite.activeRecipes=org.sample.PreconditionTest
+./mvnw rewrite:run
